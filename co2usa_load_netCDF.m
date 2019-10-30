a1f7a234739f085123e55fa4e2a21148e1738d49
Logan Mitchell <logan.e.mitchell@gmail.com>
2019-10-29 18:13:49 -0600

    Move download code to separate file

--- co2usa_load_netCDF.m
+++ co2usa_load_netCDF.m
@@ -27,21 +27,6 @@ t_overall = tic;
 currentFolder = pwd;
 readFolder = fullfile(currentFolder(1:regexp(currentFolder,'gcloud.utah.edu')+14),'data','co2-usa','synthesis_output_ornl');
 
-% Option Code to automate the download of data from the ORNL:
-ornl_download = 'n';
-if strcmp(ornl_download,'y')
-    download_location = readFolder;
-    if ~isfolder(download_location); mkdir(download_location); end
-    download_token = '<your_ORNL_download_token>'; % PUT IN YOUR DOWNLOAD TOKEN ISSUED FROM ORNL
-    hyperlinks = webread(['https://daac.ornl.gov/orders/',download_token,'/download_links.html']);
-    hyperlinks = regexp(hyperlinks,'<a href=".*?.nc">','match'); hyperlinks = replace(hyperlinks,{'<a href="','">'},'');
-    for i = 1:length(hyperlinks)
-        filename = extractAfter(hyperlinks{i},'/data/');
-        outfilename = websave(fullfile(download_location,filename),hyperlinks{i});
-    end
-    fprintf('Done downloading data from the ORNL DAAC\n')
-    clear('download_location','download_token','str','hyperlinks','filename')
-end
 
 if ~exist('cities','var')
     cities = {
--- co2usa_load_netCDF.r
+++ co2usa_load_netCDF.r
@@ -33,27 +33,6 @@ library(plotly)
 # Clear the workspace
 rm(list = ls())
 
-######## Optional code to download the data from the ORNL DAAC:  ########
-
-ornl_download = 'n'
-if (ornl_download=='y') {
-  read_folder = file.path('C:/Users','u0932260','gcloud.utah.edu','data','co2-usa','synthesis_output_ornl')
-  if (!dir.exists(read_folder)) mkdir(read_folder)
-  setwd(read_folder)
-  download_token = 'your_ORNL_download_token' # PUT IN YOUR DOWNLOAD TOKEN ISSUED FROM ORNL
-  hyperlinks = readLines(paste('https://daac.ornl.gov/orders/',download_token,'/download_links.html',sep=''))
-  hyperlinks = hyperlinks[grep(pattern='<a href=".*?.nc">',hyperlinks)] # indices of the lines with valid hyperlinks
-  hyperlinks = gsub(pattern='<li><a href="',replacement='',hyperlinks)
-  hyperlinks = gsub(pattern='\">.*</li>',replacement='',hyperlinks)
-  for (i in 1:length(hyperlinks)) {
-    filename = substr(hyperlinks[i],regexpr('/data/',hyperlinks[i])+6,nchar(hyperlinks[i]))
-    download.file(hyperlinks[i],file.path(read_folder,filename),mode='wb')
-  }
-  print('Done downloading data from the ORNL DAAC')
-  rm('download_token','hyperlinks','filename')
-}
-
-
 ######## Set the following options:  ########
 
 # City options: 'boston', 'indianapolis', 'los_angeles', 'northeast_corridor', 'portland', 'salt_lake_city', 'san_francisco_baaqmd', 'san_francisco_beacon'
